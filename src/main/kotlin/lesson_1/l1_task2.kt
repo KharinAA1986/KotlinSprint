324ab90a20c6dc7005703dd012d9917d175b4eec
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-11 16:54:22 +0300

    KS-1-2. Task 2. Variables

---
+++ src/main/kotlin/lesson_1/l1_task2.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_1
+
+fun main() {
+    var orders: Int = 75
+    val textForBuyerAfterPurchase: String = "Спасибо за покупку"
+    var workers:Int = 2000
+//    println(orders)
+//    println(textForBuyerAfterPurchase)
+    workers--
+    println(workers)
+}
