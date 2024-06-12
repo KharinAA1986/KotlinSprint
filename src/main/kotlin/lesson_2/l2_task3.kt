51d494f8589d6711729331754d164eb0780aebea
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-12 14:04:38 +0300

    KS-2-3. fix (style)

--- src/main/kotlin/lesson_2/l2_task3.kt
+++ src/main/kotlin/lesson_2/l2_task3.kt
@@ -1,11 +1,9 @@
 package org.example.lesson_2
 
-const val MINUTES_IN_HOUR:Short = 60
-const val HOURS_IN_DAY:Short = 24
+const val MINUTES_IN_HOUR: Short = 60
+const val HOURS_IN_DAY: Short = 24
 
-fun main(){
-    // т.к. сказано "– Создай целочисленные переменные и проинициализируй их этими данными;",
-    // считаю, что числа уже преобразованы и мы получаем отдельно часы и минуты
+fun main() {
     val hours: Short = 9
     val minutes: Short = 39
     val duration: Short = 457
@@ -13,5 +11,7 @@ fun main(){
     val finishTimeInMinutes = startTimeInMinutes + duration
     val finishMinute = finishTimeInMinutes % MINUTES_IN_HOUR
     val finishHour = finishTimeInMinutes / MINUTES_IN_HOUR
-    println((finishHour % HOURS_IN_DAY).toString() + ":" + finishMinute)
+    println("${format(finishHour % HOURS_IN_DAY)}:${format(finishMinute)}")
 }
+
+fun format(number: Int) = String.format("%02d", number)
