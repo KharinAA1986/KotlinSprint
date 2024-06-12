6e89a4ea74a1c434f6430d0191ddc1ebb102086e
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-12 13:55:33 +0300

    KS-2-1. fix (style)

--- src/main/kotlin/lesson_2/l2_task1.kt
+++ src/main/kotlin/lesson_2/l2_task1.kt
@@ -1,13 +1,11 @@
 package org.example.lesson_2
 
-fun main(){
+fun main() {
     val rating1 = 3
     val rating2 = 4
     val rating3 = 3
     val rating4 = 5
-    val quantity = 4
-    val method1 = (rating1 + rating2 + rating3 +rating4) * 100 / quantity
-    println(method1/100.0)
-//    val method2 = ((rating1 + rating2 + rating3 +rating4) / quantity.toFloat())
-//    println(String.format("%.2f", method2))
+    val quantity = 4.0f
+    val average = (rating1 + rating2 + rating3 + rating4) / quantity
+    println(String.format("%.2f", average))
 }
