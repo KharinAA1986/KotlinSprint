adbc8f83938e62bc83e43b89a8b012159b9cf6b7
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-12 21:08:03 +0300

    KS-3-4. Chess. fix (spaces in move)

--- src/main/kotlin/lesson_3/l3_task4.kt
+++ src/main/kotlin/lesson_3/l3_task4.kt
@@ -4,9 +4,9 @@ fun main() {
     var start = "E2"
     var finish = "E4"
     var number = 1
-    println("$start - $finish;$number")
+    println("$start-$finish;$number")
     start = "D2"
     finish = "D3"
     number++
-    println("$start - $finish;$number")
+    println("$start-$finish;$number")
 }
