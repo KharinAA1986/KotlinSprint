c69b5a68630eb3e0e28f2777071c0c37311d10bc
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-12 11:39:19 +0300

    KS-2-4. Arithmetic, game

---
+++ src/main/kotlin/lesson_2/l2_task4.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_2
+
+fun main(){
+    val buff = 20
+    val crystal = 7
+    val iron = 11
+    println("crystal: " + (crystal * buff) /100)
+    println("iron: " + (iron * buff) /100)
+}
