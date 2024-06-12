63a191e297bb4ca4c7e4d60ebfee484c43c66e5a
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-12 13:02:08 +0300

    KS-1-3. fix

--- src/main/kotlin/lesson_1/l1_task3.kt
+++ src/main/kotlin/lesson_1/l1_task3.kt
@@ -2,24 +2,19 @@ package org.example.lesson_1
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "09"
+    var minute = "07"
     println(year)
-    println(formatToTime(hour))
-    println(formatToTime(minute))
-    hour = 10
-    minute = 55
- //   println(formatToTime(hour) + ":" + formatToTime(minute))
-    print(formatToTime(hour))
+    println(hour)
+    println(minute)
+    hour = "10"
+    minute = "55"
+    print(hour)
     print(":")
-    print(formatToTime(minute))
+    print(minute)
 }
 
-fun formatToTime(number:Int): String = if(number<10) {
-        "0$number"
-    } else {
-        number.toString()
-    }
+
 
 
 
