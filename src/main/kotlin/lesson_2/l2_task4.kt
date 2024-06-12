020f230a41a5abbd45de2255a87ab04645db628b
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-12 14:07:27 +0300

    KS-2-4. fix (style)

--- src/main/kotlin/lesson_2/l2_task4.kt
+++ src/main/kotlin/lesson_2/l2_task4.kt
@@ -1,9 +1,9 @@
 package org.example.lesson_2
 
-fun main(){
+fun main() {
     val buff = 20
     val crystal = 7
     val iron = 11
-    println("crystal: " + (crystal * buff) /100)
-    println("iron: " + (iron * buff) /100)
+    println("crystal: ${(crystal * buff) / 100}")
+    println("iron: ${(iron * buff) / 100}")
 }
