2f5c06798df1b597f1c2d0637f55bccc7d58420d
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-12 13:48:13 +0300

    KS-1-5. fix

--- src/main/kotlin/lesson_1/l1_task5.kt
+++ src/main/kotlin/lesson_1/l1_task5.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_1
 
-val secondsInCosmos = 6480
-const val minutesInHour = 60
-const val secondsInMinute = 60
+const val MINUTES_IN_HOUR = 60
+const val SECONDS_IN_MINUTE = 60
 
-fun main(){
-    val hours:Int = secondsInCosmos / (minutesInHour * secondsInMinute)
-    val secondsWithoutHours:Int = secondsInCosmos % (minutesInHour * secondsInMinute)
-    val minutes:Int = secondsWithoutHours / secondsInMinute
-    val seconds:Int = secondsWithoutHours % secondsInMinute
-    println(formatToTime(hours) + ":" + formatToTime(minutes) + ":" + formatToTime(seconds))
+fun main() {
+    val secondsInCosmos = 6480
+    val hours: Int = secondsInCosmos / (MINUTES_IN_HOUR * SECONDS_IN_MINUTE)
+    val secondsWithoutHours: Int = secondsInCosmos % (MINUTES_IN_HOUR * SECONDS_IN_MINUTE)
+    val minutes: Int = secondsWithoutHours / SECONDS_IN_MINUTE
+    val seconds: Int = secondsWithoutHours % SECONDS_IN_MINUTE
+    println("${format(hours)}:${format(minutes)}:${format(seconds)}")
 }
 
-fun formatToTime(number:Int): String = if(number<10) "0$number" else number.toString()
+fun format(number: Int) = String.format("%02d", number)
