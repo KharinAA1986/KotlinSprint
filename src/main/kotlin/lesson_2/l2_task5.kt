f42c73469a1dd9ac3bf7dec156fbc7a32553e584
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-12 14:09:20 +0300

    KS-2-5. fix (style)

--- src/main/kotlin/lesson_2/l2_task5.kt
+++ src/main/kotlin/lesson_2/l2_task5.kt
@@ -2,10 +2,10 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
-fun main(){
+fun main() {
     val sum = 70000
     val percent = 16.7
     val period = 20
     val result = sum * (1 + percent / 100).pow(period)
-    println(String.format("%.3f",result))
+    println(String.format("%.3f", result))
 }
