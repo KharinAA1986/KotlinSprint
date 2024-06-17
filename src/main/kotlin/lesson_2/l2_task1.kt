8b5ca16caa0676e97b86b65a73c78f57563c674c
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-17 20:52:51 +0300

    KS-2-1. fix

--- src/main/kotlin/lesson_2/l2_task1.kt
+++ src/main/kotlin/lesson_2/l2_task1.kt
@@ -5,7 +5,7 @@ fun main() {
     val rating2 = 4
     val rating3 = 3
     val rating4 = 5
-    val quantity = 4.0f
-    val average = (rating1 + rating2 + rating3 + rating4) / quantity
+    val quantity = 4
+    val average = (rating1 + rating2 + rating3 + rating4).toFloat() / quantity
     println(String.format("%.2f", average))
 }
