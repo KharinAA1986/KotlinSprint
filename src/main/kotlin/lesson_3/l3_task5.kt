29dcdb73f3a6231e1266c14070365367df6cf52a
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-17 21:14:27 +0300

    KS-2-4. fix

--- src/main/kotlin/lesson_3/l3_task5.kt
+++ src/main/kotlin/lesson_3/l3_task5.kt
@@ -2,11 +2,10 @@ package org.example.lesson_3
 
 fun main() {
     val move = "D2-D4;0"
-    val parts = move.split(';')
-    val number = parts[1]
-    val fields = parts[0].split('-')
-    val start = fields[0]
-    val finish = fields[1]
+    val parts = move.split('-', ';')
+    val start = parts[0]
+    val finish = parts[1]
+    val number = parts[2]
     println(start)
     println(finish)
     println(number)
