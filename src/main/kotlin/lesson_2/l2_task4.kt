badaf60d55fa9f1aa7181b94d7622710abd7a550
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-17 21:09:13 +0300

    KS-2-4. fix

--- src/main/kotlin/lesson_2/l2_task4.kt
+++ src/main/kotlin/lesson_2/l2_task4.kt
@@ -4,6 +4,7 @@ fun main() {
     val buff = 20
     val crystal = 7
     val iron = 11
-    println("crystal: ${(crystal * buff) / 100}")
-    println("iron: ${(iron * buff) / 100}")
+    val percentsInOne = 100
+    println("crystal: ${(crystal * buff) / percentsInOne}")
+    println("iron: ${(iron * buff) / percentsInOne}")
 }
