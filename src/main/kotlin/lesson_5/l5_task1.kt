aeb5bcd3f959d3f91d05e78a541ad8f107fed1c5
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-18 17:52:01 +0300

    KS-5-1. fix

--- src/main/kotlin/lesson_5/l5_task1.kt
+++ src/main/kotlin/lesson_5/l5_task1.kt
@@ -1,8 +1,11 @@
 package org.example.lesson_5
 
 fun main() {
-    println("Чтобы доказать, что вы не бот, решите простой пример: 5 + 5 = ?")
+    val firstNumber = 5
+    val secondNumber = 5
+    println("Чтобы доказать, что вы не бот, решите простой пример: $firstNumber + $secondNumber = ?")
     val answer = readln().toInt()
-    if (answer == 10) println("Добро пожаловать!")
+
+    if (answer == firstNumber + secondNumber) println("Добро пожаловать!")
     else println("Доступ запрещен.")
 }
