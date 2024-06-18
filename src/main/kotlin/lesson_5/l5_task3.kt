8761fe68a7b2a010733db2821d38d8e16120bdef
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-18 18:00:42 +0300

    KS-5-3. fix

--- src/main/kotlin/lesson_5/l5_task3.kt
+++ src/main/kotlin/lesson_5/l5_task3.kt
@@ -1,6 +1,5 @@
 package org.example.lesson_5
 
-
 fun main() {
     val firstRightAnswer = 10
     val secondRightAnswer = 20
@@ -8,14 +7,14 @@ fun main() {
     val firstAnswer = readln().toInt()
     println("Введите второе число")
     val secondAnswer = readln().toInt()
-    val minAnswer = minOf(firstAnswer, secondAnswer)
-    val maxAnswer = maxOf(firstAnswer, secondAnswer)
-    val minNumber = minOf(firstRightAnswer, secondRightAnswer)
-    val maxNumber = maxOf(firstRightAnswer, secondRightAnswer)
 
-    if (minAnswer != minNumber && maxAnswer != maxNumber && maxAnswer != minNumber && minAnswer != maxNumber) {
+    if (firstAnswer != firstRightAnswer && firstAnswer != secondRightAnswer
+        && secondAnswer != secondRightAnswer && secondAnswer != firstRightAnswer
+    ) {
         println("Неудача!")
-    } else if (minAnswer == minNumber && maxAnswer == maxNumber) {
+    } else if ((firstAnswer == firstRightAnswer && secondAnswer == secondRightAnswer)
+        || (firstAnswer == secondRightAnswer && secondAnswer == firstRightAnswer)
+    ) {
         println("Поздравляем! Вы выиграли главный приз!")
     } else println("Вы выиграли утешительный приз!")
     println("Правильные числа $firstRightAnswer и $secondRightAnswer")
