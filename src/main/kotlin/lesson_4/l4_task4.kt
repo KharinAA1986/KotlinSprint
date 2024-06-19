f382aab74c326435401bd510874d467c6d70c995
KharinAA1986 <kharinaa1986@gmail.com>
2024-06-19 20:45:38 +0300

    KS-4-4. fix

--- src/main/kotlin/lesson_4/l4_task4.kt
+++ src/main/kotlin/lesson_4/l4_task4.kt
@@ -2,16 +2,12 @@ package org.example.lesson_4
 
 fun main() {
     val dayNumber = 5
-    val dayLegsIsEven = true
-    val dayBackIsEven = true
-    val dayHandsIsEven = false
-    val dayPressIsEven = false
     val dayNumberIsEven = dayNumber % 2 == 0
 
     println(
-        "Упражнения для рук:\t \t ${dayNumberIsEven == dayHandsIsEven}\n" +
-                "Упражнения для ног:\t \t ${dayNumberIsEven == dayLegsIsEven}\n" +
-                "Упражнения для спины:\t ${dayNumberIsEven == dayBackIsEven}\n" +
-                "Упражнения для пресса:\t ${dayNumberIsEven == dayPressIsEven}"
+        "Упражнения для рук:\t \t ${!dayNumberIsEven}\n" +
+                "Упражнения для ног:\t \t $dayNumberIsEven\n" +
+                "Упражнения для спины:\t $dayNumberIsEven\n" +
+                "Упражнения для пресса:\t ${!dayNumberIsEven}"
     )
 }
